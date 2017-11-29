5cf0c0a4374102d2ecd905201027e02f12d40827
masayuki.kukazawa <makmzw@masayukikukazawanoMacBook-Pro.local>
2017-11-29 17:00:15 +0900

    finish

--- Gemfile
+++ Gemfile
@@ -46,3 +46,4 @@ gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
 gem 'haml', '~> 5.0', '>= 5.0.4'
 
 gem 'jquery-rails'
+gem 'paperclip', '~> 5.1'
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,5 @@
 class Job < ApplicationRecord
     belongs_to :category
+    has_attached_file :image, styles: { large: "600×600>", medium: "300×300>", thumb: "150×150#"}
+    validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
